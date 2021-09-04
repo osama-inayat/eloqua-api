ea59a00d8173f1d7daca0c58b579f88e088a32df
Osama Inayat <83946734+osama-inayat@users.noreply.github.com>
2021-09-04 07:47:00 -0700

    initialized library

---
+++ lib/eloqua/api/wrapper.rb
@@ -0,0 +1,10 @@
+require "eloqua/api/wrapper/version"
+
+module Eloqua
+  module Api
+    module Wrapper
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/eloqua/api/wrapper/version.rb
@@ -0,0 +1,7 @@
+module Eloqua
+  module Api
+    module Wrapper
+      VERSION = "0.1.0"
+    end
+  end
+end
