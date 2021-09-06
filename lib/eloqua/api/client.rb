74ad8cca07aebbf1a956118a1e0273b4ecb7c875
Osama Inayat <83946734+osama-inayat@users.noreply.github.com>
2021-09-06 02:50:10 -0700

    added error handling architecture

--- lib/eloqua/api/client.rb
+++ lib/eloqua/api/client.rb
@@ -1,12 +1,13 @@
 require 'httparty'
 require 'helper/url_helper'
-
+require 'eloqua/api/eloqua_api_exceptions'
 module Eloqua
   module Api
     # provides single source of setting and calling apis
     class Client
       include HTTParty
       include UrlHelper
+      include EloquaApiExceptions
 
       def initialize(access_token:, base_url:, authorization_type: DEFAULT_AUTHORIZATION_TYPE)
         self.class.base_uri sub_api_version(url: base_url)
@@ -14,12 +15,31 @@ module Eloqua
       end
 
       def call(api_endpoint:, method_type: :get, params: {})
-        puts self.class.base_uri
         request_response = self.class.send(method_type, api_endpoint, body: params)
-        request_response.parsed_response
+        return request_response.parsed_response if response_successful?(request_response)
+
+        raise error_class(request_response),
+              "Code: #{request_response.code}, response: #{request_response.parsed_response}"
       end
 
       private
+
+      def error_class(response) # rubocop: disable all
+        case response.code
+        when HTTP_BAD_REQUEST_CODE
+          BadRequestError
+        when HTTP_UNAUTHORIZED_CODE
+          UnAuthorizedError
+        when HTTP_FORBIDDEN_CODE
+          ForbiddenError
+        when HTTP_NOT_FOUND_CODE
+          NotFoundError
+        when HTTP_SERVER_ERROR
+          ServerError
+        else
+          StandardError
+        end
+      end
 
       def http_headers(access_token, authorization_type)
         {
@@ -27,6 +47,12 @@ module Eloqua
           'Authorization' => format("#{authorization_type} %s", access_token),
           'Accept' => 'application/json'
         }
+      end
+
+      def response_successful?(response)
+        puts response.code
+        response.code == 200 &&
+          (!response.parsed_response.nil? || response.parsed_response != 'Not authenticated.')
       end
     end
   end
