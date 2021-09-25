1ea438c3288d0aa8d2031f12e07eb77b96c2231f
Osama Inayat <83946734+osama-inayat@users.noreply.github.com>
2021-09-25 16:08:53 +0500

    fixed url helper specs and added some new checks

--- spec/helper/url_helper_spec.rb
+++ spec/helper/url_helper_spec.rb
@@ -8,10 +8,22 @@ RSpec.describe UrlHelper do
     }
   end
 
+  let(:search_params) do
+    {
+      name: 'osama inayat',
+      currentStatus: '1'
+    }
+  end
+
   let(:class_instance) { (Class.new { include UrlHelper }).new }
 
-  it 'verifies working of \'extend_query_params\' method' do
-    response = class_instance.extend_query_params('/id', query_params)
-    expect(response).to eq('/id?page=10&count=20&')
+  it 'verifies working of \'extend_params_to_url\' method' do
+    response = class_instance.extend_params_to_url(endpoint: '/id', query_params: query_params, search_params: search_params)
+    expect(response).to eq("/id?page=10&count=20&search=name=\'*osama inayat*\'currentStatus='*1*'")
+  end
+
+  it 'verifies working of \'sub_api_version\'' do
+    response = class_instance.sub_api_version(url: TEST_SAMPLE_URL)
+    expect(response).to eq(TEST_SAMPLE_URL.gsub(/{version}/, DEFAULT_API_VERSION))
   end
 end
